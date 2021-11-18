d4b2be95561a47fdf8c877c7bf3e8299a7379e3a
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-18 19:53:15 -0300

    Criar classe ResumoContaDTO

---
+++ src/main/java/br/com/zup/gerenciadorDeContas/conta/dto/ResumoContaDTO.java
@@ -0,0 +1,45 @@
+package br.com.zup.gerenciadorDeContas.conta.dto;
+
+import br.com.zup.gerenciadorDeContas.conta.enums.Status;
+
+public class ResumoContaDTO {
+    private int id;
+    private String nome;
+    private double valor;
+    private Status status;
+
+    public ResumoContaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
